9ae3e278fbaeea23c88e40b8cc369b3b71e8e6c0
Mester, Zsolt <zsolt.mester@misys.com>
2017-03-02 22:56:11 +0100

    Display numbers in the set card view.

--- Matchismo/SetCardView.m
+++ Matchismo/SetCardView.m
@@ -34,8 +34,9 @@
 	[self setNeedsDisplay];
 }
 
+#define OFFSET_MULTIPLIER 0.3
 #define OVAL_WIDTH_MULTIPLIER 0.75
-#define OVAL_HEIGHT_MULTIPLIER 0.25
+#define OVAL_HEIGHT_MULTIPLIER 0.2
 #define DIAMOND_WIDTH_MULTIPLIER 0.1
 #define DIAMOND_HEIGHT_MULTIPLIER 0.4
 #define SQUIGGLE_WIDTH_MULTIPLIER 0.1
@@ -43,58 +44,66 @@
 
 - (void)drawCardFaceForRect:(CGRect)rect
 {
-	UIBezierPath *symbol;
-	switch (self.symbol) {
-		case SetCardViewSymbolOval:
-			symbol = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(rect.origin.x + rect.size.width * (1 - OVAL_WIDTH_MULTIPLIER) / 2,
-																	   rect.origin.y + rect.size.height * (1- OVAL_HEIGHT_MULTIPLIER) / 2,
-																	   rect.size.width * OVAL_WIDTH_MULTIPLIER,
-																	   rect.size.height * OVAL_HEIGHT_MULTIPLIER)];
-			break;
-		case SetCardViewSymbolDiamond:
-			symbol = [UIBezierPath bezierPath];
-			[symbol moveToPoint:CGPointMake(rect.origin.x + rect.size.width * DIAMOND_WIDTH_MULTIPLIER,
-										 rect.origin.y + rect.size.height / 2)];
-			[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width / 2,
-											   rect.origin.y + rect.size.height * DIAMOND_HEIGHT_MULTIPLIER)];
-			[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * (1 - DIAMOND_WIDTH_MULTIPLIER),
-											   rect.origin.y + rect.size.height / 2)];
-			[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width / 2,
-											   rect.origin.y + rect.size.height * (1 - DIAMOND_HEIGHT_MULTIPLIER))];
-			[symbol closePath];
-			break;
-		case SetCardViewSymbolSquiggle:
-			symbol = [UIBezierPath bezierPath];
-			[symbol moveToPoint:CGPointMake(rect.origin.x + rect.size.width * SQUIGGLE_WIDTH_MULTIPLIER,
-										 rect.origin.y + rect.size.height * SQUIGGLE_HEIGHT_MULTIPLIER)];
-			[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * (1 - SQUIGGLE_WIDTH_MULTIPLIER),
-											   rect.origin.y + rect.size.height * SQUIGGLE_HEIGHT_MULTIPLIER)];
-			[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * (1 - SQUIGGLE_WIDTH_MULTIPLIER),
-											   rect.origin.y + rect.size.height * (1 - SQUIGGLE_HEIGHT_MULTIPLIER))];
-			[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * SQUIGGLE_WIDTH_MULTIPLIER,
-											   rect.origin.y + rect.size.height * (1 - SQUIGGLE_HEIGHT_MULTIPLIER))];
-			[symbol closePath];
-			break;
-	}
-	[symbol addClip];
+	for (int i = 1; i <= self.number; ++i) {
+		CGFloat offset = 0;
+		if (self.number == 2) {
+			offset = (pow(-1, i)) * rect.size.height * OFFSET_MULTIPLIER;
+		} else if (self.number == 3) {
+			offset = (2 - i) * rect.size.height * OFFSET_MULTIPLIER;
+		}
 
-	switch (self.shading) {
-		case SetCardViewShadingSolid:
-			[self.color setFill];
-			[symbol fill];
-			break;
-		case SetCardViewShadingOpen:
-			[[UIColor clearColor] setFill];
-			[symbol fill];
-			[self.color setStroke];
-			[symbol stroke];
-			break;
-		case SetCardViewShadingStriped:
-			[[self.color colorWithAlphaComponent:0.25] setFill];
-			[symbol fill];
-			[self.color setStroke];
-			[symbol stroke];
-			break;
+		UIBezierPath *symbol;
+		switch (self.symbol) {
+			case SetCardViewSymbolOval:
+				symbol = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(rect.origin.x + rect.size.width * (1 - OVAL_WIDTH_MULTIPLIER) / 2,
+																		   rect.origin.y + rect.size.height * (1- OVAL_HEIGHT_MULTIPLIER) / 2 + offset,
+																		   rect.size.width * OVAL_WIDTH_MULTIPLIER,
+																		   rect.size.height * OVAL_HEIGHT_MULTIPLIER)];
+				break;
+			case SetCardViewSymbolDiamond:
+				symbol = [UIBezierPath bezierPath];
+				[symbol moveToPoint:CGPointMake(rect.origin.x + rect.size.width * DIAMOND_WIDTH_MULTIPLIER,
+												rect.origin.y + rect.size.height / 2 + offset)];
+				[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width / 2,
+												   rect.origin.y + rect.size.height * DIAMOND_HEIGHT_MULTIPLIER + offset)];
+				[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * (1 - DIAMOND_WIDTH_MULTIPLIER),
+												   rect.origin.y + rect.size.height / 2 + offset)];
+				[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width / 2,
+												   rect.origin.y + rect.size.height * (1 - DIAMOND_HEIGHT_MULTIPLIER) + offset)];
+				[symbol closePath];
+				break;
+			case SetCardViewSymbolSquiggle:
+				symbol = [UIBezierPath bezierPath];
+				[symbol moveToPoint:CGPointMake(rect.origin.x + rect.size.width * SQUIGGLE_WIDTH_MULTIPLIER,
+												rect.origin.y + rect.size.height * SQUIGGLE_HEIGHT_MULTIPLIER + offset)];
+				[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * (1 - SQUIGGLE_WIDTH_MULTIPLIER),
+												   rect.origin.y + rect.size.height * SQUIGGLE_HEIGHT_MULTIPLIER + offset)];
+				[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * (1 - SQUIGGLE_WIDTH_MULTIPLIER),
+												   rect.origin.y + rect.size.height * (1 - SQUIGGLE_HEIGHT_MULTIPLIER) + offset)];
+				[symbol addLineToPoint:CGPointMake(rect.origin.x + rect.size.width * SQUIGGLE_WIDTH_MULTIPLIER,
+												   rect.origin.y + rect.size.height * (1 - SQUIGGLE_HEIGHT_MULTIPLIER) + offset)];
+				[symbol closePath];
+				break;
+		}
+
+		switch (self.shading) {
+			case SetCardViewShadingSolid:
+				[self.color setFill];
+				[symbol fill];
+				break;
+			case SetCardViewShadingOpen:
+				[[UIColor clearColor] setFill];
+				[symbol fill];
+				[self.color setStroke];
+				[symbol stroke];
+				break;
+			case SetCardViewShadingStriped:
+				[[self.color colorWithAlphaComponent:0.25] setFill];
+				[symbol fill];
+				[self.color setStroke];
+				[symbol stroke];
+				break;
+		}
 	}
 }
 
